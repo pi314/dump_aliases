aff009abe64574dd88ac88dccfd84268dec0786c
Cychih <michael66230@gmail.com>
2014-09-30 20:29:18 +0800

    Get essential file names

--- dump_aliases.py
+++ dump_aliases.py
@@ -1,1 +1,10 @@
+import sys
+
+if len(sys.argv) < 3:
+    print('Usage:')
+    print(' ', sys.argv[0], '<etc_aliases> <name_files_dir>')
+    exit()
+
+ETC_ALIASES = sys.argv[1]
+NAME_FILES_DIR = sys.argv[2]
 
