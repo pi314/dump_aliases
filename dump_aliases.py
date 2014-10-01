63ed4aee6962e5b2753fd984819e16beeaca6aae
Cychih <michael66230@gmail.com>
2014-10-01 20:05:28 +0800

    Load aliases file

--- dump_aliases.py
+++ dump_aliases.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import re
 
 if len(sys.argv) < 3:
     print('Usage:')
@@ -8,8 +10,30 @@ if len(sys.argv) < 3:
 ETC_ALIASES = sys.argv[1]
 NAME_FILES_DIR = sys.argv[2]
 
+aliases_file = []
+
+def load_aliases_file ():
+    global aliases_file
+    for i in open(ETC_ALIASES):
+        i = re.sub(r'#.*$', '', i).strip()
+        if i == '' or i[0] == '#':
+            continue
+        if ':include:' in i:
+            l = i.split(':')
+            list_name, record_type, name_file = l[0], 'file', l[-1].split('/')[-1]
+            aliases_file.append( (list_name, record_type, name_file) )
+        else:
+            l = i.split(':')
+            list_name = l[0]
+            emails = l[-1].split(',')
+            for j in emails:
+                aliases_file.append( (list_name, 'mail', j.strip()) )
+
 def main ():
-    pass
+    load_aliases_file()
+    for i in aliases_file:
+        print(i)
 
 if __name__ == '__main__':
     main()
+
