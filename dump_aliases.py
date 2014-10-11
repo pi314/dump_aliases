f4ef764c576be63d2521e8c9fd7420caecd72765
Apua <Apua.A.Aa@gmail.com>
2014-10-11 11:12:25 +0800

    Rewritten functions

--- dump_aliases.py
+++ dump_aliases.py
@@ -7,6 +7,7 @@ background:
 
 issues:
   - store all error/warning(s) and output it.
+  - consider '\"'
 
 cleaning rules:
   1. '#.*$' should be ignored
@@ -26,23 +27,29 @@ key verifying rules:
 import re
 
 
-def cleaned(content):
-  r"""
-  >>> content = '\na  :  b  ,c:,  "  ,  \n  : d"\t\r\n\r e\n'
-  >>> cleaned(content)
-  'a:b, c:, ", :d" e\n'
+def get_cleaned_content(file_path):
   """
-  def subclean(m):
-    s = m.group()
-    s = re.sub(r' +', ' ', s)
-    s = re.sub(r' ?: ?', ':', s)
-    s = re.sub(r' ?, ?', ', ', s)
-    return s
-  content = re.sub(r'#.*$', '', content, flags=re.M)
-  content = re.sub(r'\n\s|[\t\r\f\v]', ' ', content)
-  content = re.sub(r'(.*)("[^"]*")?', subclean, content)
-  content = re.sub(r'^\s+', '', content)
-  return content
+  file_path: str -> cleaned lines: list
+  """
+  def cleaned(content):
+    r"""
+    >>> content = '\na  :  b  ,c:,  "  ,  \n  : d"\t\r\n\r e\n'
+    >>> cleaned(content)
+    'a:b, c:, ", :d" e\n'
+    """
+    def subclean(m):
+      s = m.group()
+      s = re.sub(r' +', ' ', s)
+      s = re.sub(r' ?: ?', ':', s)
+      s = re.sub(r' ?, ?', ', ', s)
+      return s
+    content = re.sub(r'#.*$', '', content, flags=re.M)
+    content = re.sub(r'\n\s|[\t\r\f\v]', ' ', content)
+    content = re.sub(r'(.*)(".*?(?<!\\)")?', subclean, content)
+    content = re.sub(r'^\s+', '', content)
+    return content
+
+  return cleaned(open(file_path).read()).splitlines()
 
 
 def clean_key(key):
@@ -53,59 +60,54 @@ def clean_key(key):
   return re.sub(r'"([^"]*)"', lambda m: m.group(1) or ' ', key)
 
 
-def split2kv(line):
-  '''
-  only use for aliases file but other included files
-  str -> [ {"key": str, "values": str} ]
-  '''
-  kv_patt = r"^\s*(?P<key>\S+?)\s*:\s*(?P<values>.+?)\s*$"
-  #print(repr(line))
-  result = re.findall(kv_patt, line, flags=re.M)
-  #print(result)
-  return result
-
-
 def split_values(values):
-  '''
-  only use for included files
-  str -> [ value ]
-  '''
-  #print('--->', values)
+  """
+  cleaned values: str -> list
+  >>> split_values('b, c:, ", :d" e, :include:/dev/null')
+  ['b', 'c:', '", :d"', 'e']
+  """
   result = []
-  for value in re.split(r"\s*(?:[,\n])\s*", values.strip()):
-    m = re.match(r":include:\s*(?P<path>.*)", value)
-    if m:
-      subcontent = get_content(m.groupdict()["path"])
-      result.extend(split_values(cleaned(subcontent)))
+  for value in re.findall(r'".*?(?<!\\)"|[^, ]+', values):
+    if value.startswith(':include:'):
+      cleaned = get_cleaned_content(value[9:])
+      result.extend(split_values(' '.join(cleaned)))
     else:
       result.append(value)
-  #print(result)
   return result
 
 
-def get_content(path):
-  return open(path).read()
+def split_alias(record):
+  """
+  "key:values" -> (key, values)
+  """
+  key, values = record.split(':', 1)
+  return clean_key(key), split_values(values)
 
 
-def main(aliases_path):
-  aliases_content = get_content(aliases_path)
-  #print(aliases_content)
-  cleaned_content = cleaned(aliases_content)
-  #print(cleaned_content)
-  result = []
-  for key, values in split2kv(cleaned_content):
-    values = split_values(values)
-    for value in values:
-      result.append((key, value))
-  result.sort()
-  #__import__('pprint').pprint(result)
-  return result
+def parse_include_file(file_path):
+  """
+  _ -> values: [value]
+  """
+  return split_values(get_cleaned_include_file(file_path))
+
+
+def parse_aliases_file(aliases_path):
+  """
+  _ -> aliases: {key: [value]}
+  """
+  aliases = get_cleaned_content(aliases_path)
+  return {k:vs for k,vs in map(split_alias, aliases)}
 
 
 if __name__=='__main__':
-
   from argparse import ArgumentParser
   parser = ArgumentParser()
   parser.add_argument('aliases_path')
   aliases_path = parser.parse_args().aliases_path
-  print(main(aliases_path))
+
+  parsed_data = parse_aliases_file(aliases_path)
+  for k,v in sorted(
+    ((k,v) for k,vs in parsed_data.items() for v in vs),
+    key=lambda t:t
+    ):
+    print("%s:%s"%(k,v))
