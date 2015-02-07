a780d0b51c15b1cdabe33bfcd8b439dc7fb2345f
Cychih <michael66230@gmail.com>
2015-02-07 18:15:38 +0800

    when a name file ref. another name file, check if it is a mailing list

--- dump_aliases.py
+++ dump_aliases.py
@@ -2,14 +2,26 @@ import sys
 import os
 import re
 
-ETC_ALIASES = sys.argv[1]
-NAME_FILES_DIR = sys.argv[2]
+ETC_ALIASES = ''
+NAME_FILES_DIR = ''
+
+FILE = 'file'
+MAIL = 'mail'
 
 aliases_file = []
 list_relation = {}
+file_table = {}
 db = {}
 
 def load_aliases_file ():
+    '''
+        Load ETC_ALIASES file and parse it in ``aliases_file`` variable
+        format:
+            (list_name, record_type, data)
+            list_name: (string) the name of mailing list
+            record_type: (string) "file" or "mail"
+            data: (string) an email address or a file name
+    '''
     global aliases_file
 
     for i in open(ETC_ALIASES):
@@ -19,23 +31,33 @@ def load_aliases_file ():
 
         if ':include:' in i:
             l = i.split(':')
-            list_name, record_type, name_file = l[0], 'file', l[-1].split('/')[-1]
+            list_name, record_type, name_file = l[0], FILE, l[-1].split('/')[-1].strip()
             aliases_file.append( (list_name, record_type, name_file) )
+            file_table[name_file] = list_name
 
         else:
             l = i.split(':')
             list_name = l[0]
             emails = l[-1].split(',')
             for j in emails:
-                aliases_file.append( (list_name, 'mail', j.strip()) )
+                aliases_file.append( (list_name, MAIL, j.strip()) )
 
 def setup_list_relation ():
+    '''
+        Set up the relation between mailing lists
+        type(list_relation) == dict
+        for i in list_relation: type(i) == set
+    '''
     global list_relation
 
     for i in aliases_file:
         list_relation[ i[0] ] = set()
 
 def add_to_db (list_name, data):
+    '''
+        ``db`` is a dictionary
+        list -> email address
+    '''
     global db
     data = data.strip()
 
@@ -55,6 +77,8 @@ def process_name_file (list_name, file_name, stack):
 
         if ':include:' in i:
             sub_file_name = i.split('/')[-1]
+            if sub_file_name in file_table:
+                list_relation[list_name].add( file_table[sub_file_name] )
             process_name_file(list_name, sub_file_name, stack + [file_name])
 
         elif '@' in i:
@@ -67,12 +91,14 @@ def process_name_file (list_name, file_name, stack):
             add_to_db(list_name, i)
 
 def process_list_record (record):
+    global file_table
+
     list_name, record_type, data = record
 
-    if record_type == 'file':
+    if record_type == FILE:
         process_name_file(list_name, data, [])
 
-    elif record_type == 'mail':
+    elif record_type == MAIL:
         add_to_db(list_name, data)
 
     else:
@@ -112,11 +138,16 @@ def apply_relations (main_list_name, stack):
     return ret
 
 def main ():
+    global ETC_ALIASES
+    global NAME_FILES_DIR
 
     if len(sys.argv) < 3:
         print('Usage:')
-        print(' ', sys.argv[0], '<etc_aliases> <name_files_dir>')
+        print(' ', sys.argv[0], '[etc-aliases-file] [name-files-dir]')
         exit()
+
+    ETC_ALIASES    = sys.argv[1]
+    NAME_FILES_DIR = sys.argv[2]
 
     load_aliases_file()
     setup_list_relation()
